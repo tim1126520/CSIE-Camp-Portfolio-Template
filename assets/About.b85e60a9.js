b4bb83f82d7e560a52ebc56377649d3d95b19146
tim1126520 <tim1126520@users.noreply.github.com>
2022-07-07 09:05:52 +0000

    deploy: 1ee9a19c22064cd69aeaf3054cdade91cf9db2c4

---
+++ assets/About.b85e60a9.js
@@ -0,0 +1,4 @@
+import{_ as s}from"./Typing.edf4b902.js";import{r as a,o,c as m,b as l,d as n}from"./index.3a992ec5.js";const p={class:"h-full w-full px-8 pt-8 sm:px-12 sm:pt-12 lg:px-16 lg:pt-16"},f={__name:"About",setup(u){const e=a(0);return(x,t)=>(o(),m("div",p,[e.value>=0?(o(),l(s,{key:0,onDone:t[0]||(t[0]=r=>e.value++),text:"\u731C\u731C\u6211\u662F\u8AB0",class:"mb-8 block text-2xl sm:text-3xl lg:text-4xl"})):n("",!0),e.value>=1?(o(),l(s,{key:1,onDone:t[1]||(t[1]=r=>e.value++),text:`\u6211\u662F\u611B\u7684\u5C0F\u5C4B\u7BA1\u7406\u4EBA Dr.Tim
+        \u6211\u770B\u4F60\u662F\u4E0D\u61C2\u5594
+        \u8B93\u6211\u4F86\u6559\u6559\u4F60\u5427
+        \u59CB\u65BC\u611B\u7684\u8AB2\u7A0B`})):n("",!0)]))}};export{f as default};
